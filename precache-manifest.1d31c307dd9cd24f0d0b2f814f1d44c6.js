fe24543da92c350fb1fa347090b2038f0ce63d72
Taher Hamzaoui <taher.hamzaoui@esprit.tn>
2020-10-08 16:06:17 +0200

    Updates

---
+++ precache-manifest.1d31c307dd9cd24f0d0b2f814f1d44c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6019e583dbec5ba9d3c6708e344570d3",
+    "url": "/taherhamzaouii/Airbnb-React-/index.html"
+  },
+  {
+    "revision": "d86c352ac54355233216",
+    "url": "/taherhamzaouii/Airbnb-React-/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "190c277acc8298a7e466",
+    "url": "/taherhamzaouii/Airbnb-React-/static/css/main.9d1497ca.chunk.css"
+  },
+  {
+    "revision": "d86c352ac54355233216",
+    "url": "/taherhamzaouii/Airbnb-React-/static/js/2.3b3e81f9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/taherhamzaouii/Airbnb-React-/static/js/2.3b3e81f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190c277acc8298a7e466",
+    "url": "/taherhamzaouii/Airbnb-React-/static/js/main.9e89b5ee.chunk.js"
+  },
+  {
+    "revision": "0e2f6da537c2a5647449",
+    "url": "/taherhamzaouii/Airbnb-React-/static/js/runtime-main.3df115ad.js"
+  }
+]);
